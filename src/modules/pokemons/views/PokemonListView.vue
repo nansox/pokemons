c49fe9591da4bb25e74d87cdcc91b0e39b07b455
Antonio Garcia <antorri949@gmail.com>
2023-03-22 10:54:30 +0100

    :feat #pk-button implemeted

--- src/modules/pokemons/views/PokemonListView.vue
+++ src/modules/pokemons/views/PokemonListView.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PkButton from '../../../common/components/pk-button.vue'
+</script>
 
 <template>
-  <div><h1>Pokemons list page</h1></div>
+  <div class="pokemons-list">
+    <h1>Pokemons list page</h1>
+    <PkButton>Test</PkButton>
+  </div>
 </template>
+
+<style scoped>
+.pokemons-list {
+  height: 100%;
+  background-color: var(--light-grey);
+  padding: 4rem;
+}
+</style>
